76c6f84380f8586bde54436a58338df2aea8921a
eQuechen <181157021+eQuechen@users.noreply.github.com.>
2026-02-06 15:43:32 +0000

    feat(ui): add PlannerForm component for dive planning inputs

---
+++ components/planner-form.tsx
@@ -0,0 +1,51 @@
+import {
+    Field,
+    FieldContent,
+    FieldDescription,
+    FieldError,
+    FieldGroup,
+    FieldLabel,
+    FieldLegend,
+    FieldSeparator,
+    FieldSet,
+    FieldTitle,
+} from "@/components/ui/field"
+
+import { Input } from "./ui/input"
+import { Switch } from "./ui/switch"
+import { Button } from "./ui/button";
+
+export function PlannerForm() {
+    return (
+        <FieldSet>
+            <FieldLegend>RDP digital</FieldLegend>
+            <FieldDescription>⚠️ NO válida para planificación real de inmersiones.</FieldDescription>
+            <FieldGroup>
+                <Field orientation="horizontal" className="w-fit">
+                    <Switch id="switch" />
+                    <FieldLabel htmlFor="switch">Inmersión sucesiva</FieldLabel>
+                </Field>
+
+                <Field>
+                    <FieldLabel htmlFor="depth">Profundidad máxima (m)</FieldLabel>
+                    <Input type="number" id="depth" autoComplete="off" placeholder="ej: 18" />
+                    <FieldDescription></FieldDescription>
+                </Field>
+
+                <Field>
+                    <FieldLabel htmlFor="bottom-time">Tiempo de fondo (min)</FieldLabel>
+                    <Input type="number" id="bottom-time" autoComplete="off" placeholder="ej: 35" />
+                    <FieldDescription></FieldDescription>
+                </Field>
+
+                <Field orientation="horizontal">
+                    <Button type="submit">Calcular NDL</Button>
+                    <Button variant="outline" type="button">
+                        Borrar
+                    </Button>
+                </Field>
+            </FieldGroup>
+        </FieldSet>
+    );
+}
+
